2d28ea069445b6a71af08defda819a7209b3891f
Jun Rhy Crodua <jrcrodua@gmail.com>
2022-06-27 13:11:12 +0800

    first commit

---
+++ database/migrations/2022_06_23_141702_create_leads_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leads', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid')->unique();
+            $table->string('phone_number');
+            $table->string('email_address');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('dob');
+            $table->tinyText('gender');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leads');
+    }
+};
